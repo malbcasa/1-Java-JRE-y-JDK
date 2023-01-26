27978fa52c2a986877ebdfdad4b91f11f2a1aafc
Mauricio <mauriciocasarottocad@outlook.com.ar>
2023-01-25 23:41:35 -0300

    Aula 04 completada

---
+++ java-segundos-pasos/src/TipoVariable.java
@@ -0,0 +1,34 @@
+
+public class TipoVariable {
+	public static void main(String[] args) {
+		/*System.out.println("Hola mundo!!!");
+		
+		int edad = 28;
+		System.out.println(edad);
+		
+		edad = 46 + 88;
+		System.out.println("Mi edad es " + edad);
+		
+		double salario = 1250.56;
+		System.out.println(salario);
+		
+		double edad = 28;
+		double salarioMitad = salario/2;
+		System.out.println(salarioMitad);
+		
+		int division = 1250/3;
+		System.out.println(division);*/
+		
+		double variable1 = 230.89;
+		int variable1Entero = (int) variable1; //Cast
+		System.out.println(variable1Entero);
+		
+		long prueba = 1222222222222222222L;
+		short numeroPequeno = 13555;
+		byte numeroAunMasPequeno = 15;
+		float numeroDecimalPequeno = 2.5F;
+		
+		int resultado = (int) variable1 + variable1Entero;
+		System.out.println(resultado);
+	}
+}
